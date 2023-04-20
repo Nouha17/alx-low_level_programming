3ef2b18025e15faf03529dde2d6cbdecf9045693
Nouha El Hassouni <noha.nonoss3@gmail.com>
2023-04-19 19:01:30 -0700

    poka

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include "function_pointers.h"
+
 /**
  * int_index - searches for an integer in an array
  * @array: pointer to the array
@@ -12,12 +14,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array && cmp && size > 0)
+
 	{
 		for (i = 0; i < size; i++)
+
 		{
 			if (cmp(array[i]) != 0)
 				return (i);
 		}
 	}
+
 	return (-1);
+
 }
