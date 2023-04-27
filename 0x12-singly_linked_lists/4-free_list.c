5c96d21a52ea0c6ecd9229cc74f228deaeb50d1c
Nouha El Hassouni <noha.nonoss3@gmail.com>
2023-04-27 14:42:52 -0700

    pol

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+*free_list - frees a list
+*@head: pointer to the first node
+*/
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+	int i;
+
+	for (i = 0; head != NULL; i++)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}
+
