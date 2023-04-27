02d4e51c30f88810f21ee527db7719c477926abd
Nouha El Hassouni <noha.nonoss3@gmail.com>
2023-04-27 14:34:58 -0700

    epol

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+*add_node - adds a new noe at the beginnigng of a list
+*@head: pointer to beginning of a list
+*@str: value of the node to add
+*Return: the address of the new element
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	char *newstr;
+
+	new = malloc(sizeof(list_t));
+	if (new == 0)
+		return (0);
+	newstr = strdup(str);
+	if (!newstr)
+	{
+		free(new);
+		return (0);
+	}
+
+	new->str = newstr;
+	new->len = strlen(str);
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
