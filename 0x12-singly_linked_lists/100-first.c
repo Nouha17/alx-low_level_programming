709df97d9dce036bf4ac414e9c2a5172b247a8dd
Nouha El Hassouni <noha.nonoss3@gmail.com>
2023-04-28 16:41:22 -0700

    ro

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * *premain - premain entry
- * */
-
-void __attribute__((constructor)) premain()
+ *  * before_main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+void __attribute__((constructor)) before_main(void)
 {
 		printf("You're beat! and yet, you must allow,\n");
 			printf("I bore my house upon my back!\n");
