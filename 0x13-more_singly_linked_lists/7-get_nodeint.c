98714b4d0aa0b46d4d08d3ff4e972ce14a1d4170
Nouha El Hassouni <noha.nonoss3@gmail.com>
2023-05-02 12:21:07 -0700

    yu

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,17 +11,17 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 		unsigned int i = 0;
 
-			if (head == NULL)
-						return (NULL);
+		if (head == NULL)
+			return (NULL);
 
-				while ((i < index) && head != NULL)
-						{
-									head = head->next;
-											i++;
-												}
+		while ((i < index) && head != NULL)
+		{
+			head = head->next;
+			i++;
+		}
 
-					if (i == index)
-								return (head);
+		if (i == index)
+			return (head);
 
-						return (NULL);
+		return (NULL);
 }
