8e8f5b1e0f638d69c253fdce65dd73ec5b254329
Nouha El Hassouni <noha.nonoss3@gmail.com>
2023-05-02 11:41:06 -0700

    op

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * *get_nodeint_at_index - returns the nth node of a list
- * *@head: pointer to the first node
- * *@index: the position of the node
- * *Return: the nth node
- * */
+  *get_nodeint_at_index - returns the nth node of a list
+  *@head: pointer to the first node
+  *@index: the position of the node
+  *Return: the nth node
+  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
